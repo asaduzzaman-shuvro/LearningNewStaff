102eb2fe88237628c413661756359cb647417116
Asaduzzaman Shuvro <asaduzzaman.duet@gmail.com>
2024-08-26 15:50:56 +0600

    InteractiveButtonWidget app created

---
+++ InteractiveButtonInWidget/InteractiveButtonInWidget/Intentents/AppIntents.swift
@@ -0,0 +1,10 @@
+//
+//  AppIntents.swift
+//  InteractiveButtonInWidget
+//
+//  Created by Asaduzzaman Shuvro on 26/8/24.
+//
+
+import Intents
+
+
---
+++ InteractiveButtonInWidget/InteractiveButtonInWidget/InteractiveButtonInWidgetApp.swift
@@ -0,0 +1,20 @@
+//
+//  InteractiveButtonInWidgetApp.swift
+//  InteractiveButtonInWidget
+//
+//  Created by Asaduzzaman Shuvro on 26/8/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveButtonInWidgetApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .defaultAppStorage(UserDefaults(suiteName: "group.interactivebuttonwidget")!)
+        }
+    }
+}

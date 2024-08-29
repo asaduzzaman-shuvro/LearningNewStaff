92b402dfd3e84b2b3260f33a4b7f1cf69410c651
Asaduzzaman Shuvro <asaduzzaman.duet@gmail.com>
2024-08-29 12:58:58 +0600

    Interactive button widget extension added

---
+++ InteractiveButtonInWidget/Interactive/Interactive.swift
@@ -0,0 +1,80 @@
+//
+//  Interactive.swift
+//  Interactive
+//
+//  Created by Asaduzzaman Shuvro on 26/8/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), emoji: "😀")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct InteractiveEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Time:")
+            Text(entry.date, style: .time)
+
+            Text("Emoji:")
+            Text(entry.emoji)
+        }
+    }
+}
+
+struct Interactive: Widget {
+    let kind: String = "Interactive"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                InteractiveEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                InteractiveEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    Interactive()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
---
+++ InteractiveButtonInWidget/Interactive/InteractiveBundle.swift
@@ -0,0 +1,16 @@
+//
+//  InteractiveBundle.swift
+//  Interactive
+//
+//  Created by Asaduzzaman Shuvro on 26/8/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct InteractiveBundle: WidgetBundle {
+    var body: some Widget {
+        Interactive()
+    }
+}

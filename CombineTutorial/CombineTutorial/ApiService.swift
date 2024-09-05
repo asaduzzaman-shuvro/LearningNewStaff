7af36ef3bd3c1a8f720191cbb09845cb31689e80
Asaduzzaman Shuvro <asaduzzaman.duet@gmail.com>
2024-09-05 17:23:05 +0600

    CombineTutorial project added

---
+++ CombineTutorial/CombineTutorial/ApiService.swift
@@ -0,0 +1,8 @@
+//
+//  ApiService.swift
+//  CombineTutorial
+//
+//  Created by Asaduzzaman Shuvro on 5/9/24.
+//
+
+import Foundation
---
+++ CombineTutorial/CombineTutorial/CombineTutorialApp.swift
@@ -0,0 +1,20 @@
+//
+//  CombineTutorialApp.swift
+//  CombineTutorial
+//
+//  Created by Asaduzzaman Shuvro on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineTutorialApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

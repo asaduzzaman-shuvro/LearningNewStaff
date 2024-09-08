03e13a07d799156bc3b0d5897b99c47c447209dd
Asaduzzaman Shuvro <asaduzzaman.duet@gmail.com>
2024-09-08 23:08:26 +0600

    CombineViewController added

---
+++ CombineTutorial/CombineTutorial/CombineViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CombineViewController.swift
+//  CombineTutorial
+//
+//  Created by Asaduzzaman Shuvro on 6/9/24.
+//
+
+import UIKit
+import Combine
+
+class CombineViewController: UIViewController {
+    private var currentValue = 0
+
+    private let label = UILabel()
+    private var anyCancellable = Set<AnyCancellable>()
+    private let publisher = PassthroughSubject<String, Never>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let button = UIButton()
+        button.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: 200)
+        button.backgroundColor = .red
+        button.addTarget(self, action: #selector(didPressOnButton(sender:)), for: .touchUpInside)
+        view.addSubview(button)
+        
+        label.text = "The value is: \(currentValue)"
+        label.frame = CGRect(x: 0, y: 200, width: 200, height: 100)
+        view.addSubview(label)
+        
+    }
+    
+    func bind() {
+        publisher
+            .map { "The value is:  \($0)" }
+            .assign(to: \.text!, on: label)
+            .store(in: &anyCancellable)
+    }
+    
+    @objc private func didPressOnButton(sender: Any) {
+        currentValue += 1
+        publisher.send("\(currentValue)")
+    }
+}

44cb4e757d6c5d74199f0bc58acaf8b7402fe05c
Asaduzzaman Shuvro <asaduzzaman.duet@gmail.com>
2024-09-08 23:33:00 +0600

    RefactoredCombineViewController added

--- CombineTutorial/CombineTutorial/RefactoredCombineViewController.swift
+++ CombineTutorial/CombineTutorial/RefactoredCombineViewController.swift
@@ -1,31 +1,30 @@
 //
-//  CombineViewController.swift
+//  RefactoredCombineViewController.swift
 //  CombineTutorial
 //
-//  Created by Asaduzzaman Shuvro on 6/9/24.
+//  Created by SHUVRO on 9/8/24.
 //
 
 import UIKit
 import Combine
-import SwiftUI
 
-class CombineViewController: UIViewController {
-    private var currentValue = 0
+class RefactoredCombineViewController: UIViewController {
 
     private let label = UILabel()
     private var anyCancellable = Set<AnyCancellable>()
     private let publisher = PassthroughSubject<String, Never>()
+    private let counter = Counter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let button = UIButton()
         button.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: 200)
         button.backgroundColor = .red
         button.addTarget(self, action: #selector(didPressOnButton(sender:)), for: .touchUpInside)
         view.addSubview(button)
         
-        label.text = "The value is: \(currentValue)"
+        label.text = counter.value
         label.frame = CGRect(x: 0, y: 200, width: 200, height: 100)
         view.addSubview(label)
         
@@ -33,27 +32,27 @@ class CombineViewController: UIViewController {
     }
     
     private func bind() {
-        publisher
-            .map { "The value is:  \($0)" }
+        counter.$value
             .assign(to: \.text!, on: label)
             .store(in: &anyCancellable)
+//        publisher
+//            .map { "The value is:  \($0)" }
+//            .assign(to: \.text!, on: label)
+//            .store(in: &anyCancellable)
     }
     
     @objc private func didPressOnButton(sender: Any) {
-        currentValue += 1
-        publisher.send("\(currentValue)")
+        counter.increment()
     }
 }
 
-struct CombineView: UIViewControllerRepresentable {
-    typealias UIViewControllerType = RefactoredCombineViewController
 
-    func makeUIViewController(context: Context) -> UIViewControllerType {
-        return RefactoredCombineViewController()
-    }
+class Counter {
+    @Published private (set) var value: String = "The value is 0"
+    private var current = 0
     
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-        
+    func increment() {
+        current += 1
+        value = "The value is \(current)"
     }
-    
 }
